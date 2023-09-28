fc34b1803ddb3a3165e3f2097f9c3e3edd3cf748
Durgesh Desai <durgeshwebdev@gmail.com>
2023-09-28 15:16:46 +0530

    first commit

---
+++ crud-app/src/Component/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+    <>
+    <h1>HOme Component</h1>
+    </>
+    );
+    }
+    export default Home;
+    
---
+++ crud-app/src/Component/NotFound.js
@@ -0,0 +1,7 @@
+import notfound from '../Assets/images/pexels-pixabay-36717.jpg';
+const NotFound = () => {
+return (
+<img src={notfound} style={{width: '30%', margin: '80px 0 0 35%'}}/>
+)
+}
+export default NotFound;
